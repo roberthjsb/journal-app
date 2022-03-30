5cbe63ff707849edc6e6b3c592c14b7fc8e4e3ba
Roberth Salazar <roberth.salazar@gmail.com>
2022-03-30 18:12:59 -0300

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC8rdzo358Mna9INsPm8zllaYOcodpDdCs",
-    authDomain: "journalapp-7add5.firebaseapp.com",
-    databaseURL: "https://journalapp-7add5.firebaseio.com",
-    projectId: "journalapp-7add5",
-    storageBucket: "journalapp-7add5.appspot.com",
-    messagingSenderId: "662384351699",
-    appId: "1:662384351699:web:f739ddcd851e3ef947b726"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
