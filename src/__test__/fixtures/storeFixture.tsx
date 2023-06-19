740e68a39b2a0a74f93130cb00593dee70ce2f3a
roberthjsb <roberth.salaza@gmail.com>
2023-06-18 20:06:58 -0400

    fix storeFixture

--- src/__test__/fixtures/storeFixture.tsx
+++ src/__test__/fixtures/storeFixture.tsx
@@ -3,8 +3,8 @@ import React, { ReactElement } from "react";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 
-import { authSlice } from "../../src/store";
-import { journalSlice } from "../../src/store/journal/journalSlice";
+import { authSlice } from "../../store/auth/authSlice";
+import { journalSlice } from "../../store/journal/journalSlice";
 import { journalTestWithInfo } from "./journalFixture";
 
 export const testStore = (state: any) =>
